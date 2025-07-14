3ffc184bbde8af238ec4202e9fb3ae5ff3e5f7cb
InvalidSunX <rval2359@gmail.com>
2025-07-14 12:04:13 -0400

    Made working asteroids

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,15 +8,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
     clock = pygame.time.Clock()
-    dt = (clock.tick(60) / 1000)
-    while 0 < 10:
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+
+    while running:
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        screen.fill(color=0)
+                running = False
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
 
 
 if __name__ == "__main__":
