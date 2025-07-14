7592b676774fb2cbffc4c2b0e3cbcd23f8959ff2
InvalidSunX <rval2359@gmail.com>
2025-07-14 11:48:05 -0400

    Added fps

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = (clock.tick(60) / 1000)
     while 0 < 10:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=0)
         pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
